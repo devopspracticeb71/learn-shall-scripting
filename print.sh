33c32aa1594725ee1a65e9561b1dd3c28064458a
sree <ec2-user@ip-172-31-23-120.ec2.internal>
2023-06-26 11:54:01 +0000

    print and colours

---
+++ print.sh
@@ -0,0 +1,17 @@
+#Print somthing is, we have many commands in shall but echo is widelly using
+
+echo hello world
+
+# while printing somthing, to grab users attention, we can use colours
+#Syntax echo -e "\e[COLmMESSAGE\e[0m"
+#-e to enable colours
+#\e[COLm - to enable certain colours
+#\e[0m - to disable colours which is enabled
+#COL stands for colours and possiable colours are RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGENTA(35), CYAN(36)
+
+echo -e "\e[31mHello in Red colour\e[0m"
+echo -e "\e[31mHello in GREEN colour\e[0m"
+echo -e "\e[31mHello in YELLOW colour\e[0m"
+echo -e "\e[31mHello in BLUE colour\e[0m"
+echo -e "\e[31mHello in MAGENTA colour\e[0m"
+echo -e "\e[31mHello in CYAN colour\e[0m"
