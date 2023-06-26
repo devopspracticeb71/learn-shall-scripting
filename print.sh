e305f6f63aa9182816b667e4a6bc7535f37bceb6
sree <ec2-user@ip-172-31-23-120.ec2.internal>
2023-06-26 11:57:13 +0000

    print and colours2

--- print.sh
+++ print.sh
@@ -10,8 +10,8 @@ echo hello world
 #COL stands for colours and possiable colours are RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGENTA(35), CYAN(36)
 
 echo -e "\e[31mHello in Red colour\e[0m"
-echo -e "\e[31mHello in GREEN colour\e[0m"
-echo -e "\e[31mHello in YELLOW colour\e[0m"
-echo -e "\e[31mHello in BLUE colour\e[0m"
-echo -e "\e[31mHello in MAGENTA colour\e[0m"
-echo -e "\e[31mHello in CYAN colour\e[0m"
+echo -e "\e[32mHello in GREEN colour\e[0m"
+echo -e "\e[33mHello in YELLOW colour\e[0m"
+echo -e "\e[34mHello in BLUE colour\e[0m"
+echo -e "\e[35mHello in MAGENTA colour\e[0m"
+echo -e "\e[36mHello in CYAN colour\e[0m"
